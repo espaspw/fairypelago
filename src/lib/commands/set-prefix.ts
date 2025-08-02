7076fff689d15a4eb3da93eb5f8d0c8992702fb9
espaspw <superpanamaworld@gmail.com>
2025-08-01 20:16:15 -0700

    Add set prefix command

---
+++ src/lib/commands/set-prefix.ts
@@ -0,0 +1,27 @@
+import { Command } from '../../types/command'
+import * as DB from '../../db/db'
+
+import { PermissionFlagsBits } from 'discord.js'
+
+const setPrefix: Command = {
+  name: 'Set Prefix',
+  aliases: ['prefix', 'setprefix'],
+  categories: ['Utility', 'Admin'],
+  description: 'Sets the command prefix for the current guild.',
+  helpMessage: 'prefix `new-prefix`',
+  async execute(message, tokens) {
+    if (!message.member?.permissions.has(PermissionFlagsBits.Administrator)
+        && message.author.id !== process.env.OWNER_ID) {
+      await message.reply('Only admins can use this command.')
+      return;
+    }
+    if (tokens[0] === undefined) {
+      await message.reply(`Current prefix is (${DB.getCommandPrefix(message.guildId)}).`)
+    } else {
+      await DB.setCommandPrefix(message.guildId, tokens[0])
+      await message.reply(`Prefix has been set to (${tokens[0]}).`)
+    }
+  },
+}
+
+export default setPrefix
