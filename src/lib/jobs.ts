fb221f0d1cb9d498c0cee47dac1ed0c88d55a572
espaspw <superpanamaworld@gmail.com>
2026-03-09 15:03:38 -0700

    Handle job initialization when jobs folder is missing

--- src/lib/jobs.ts
+++ src/lib/jobs.ts
@@ -10,14 +10,18 @@ let jobs: Jobs = {}
 
 export async function loadJobs () {
   jobs = {}
-  const files = await fs.readdir(path.join(import.meta.dirname, 'jobs'))
-  for (const file of files) {
-    try {
-      const job = (await import(`./jobs/${file.replace('.ts', '')}`)).default as Job
-      jobs[job.name] = job
-    } catch (err) {
-      console.error(err)
+  try {
+    const files = await fs.readdir(path.join(import.meta.dirname, 'jobs'))
+    for (const file of files) {
+      try {
+        const job = (await import(`./jobs/${file.replace('.ts', '')}`)).default as Job
+        jobs[job.name] = job
+      } catch (err) {
+        logger.error(err)
+      }
     }
+  } catch (err) {
+    logger.warn('No jobs/ directory detected. Skipping.')
   }
 }
 
