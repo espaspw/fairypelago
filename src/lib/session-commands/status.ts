932c4aa595f5bde6c4c1fbde203f2d55fc78acd8
espaspw <superpanamaworld@gmail.com>
2026-04-25 23:03:18 -0700

    Change status command to make it a bit fancier
    * Now prints static state while fetching dynamic status
    This is sort of experimental to increase the feeling of responsiveness
    from the bot since the webhost fetch can sometimes take a few seconds.
    * Expanded header with total progress bar
    * Game name for player data
    * Only prints player data when <= 8 players. Pagination might be a hassle
    to use anyway for a quick status check, although plan to implement a
    status <slotname> subcommand for specific player status

--- src/lib/session-commands/status.ts
+++ src/lib/session-commands/status.ts
@@ -1,67 +1,155 @@
 import { SessionCommand } from '../../types/session-command.js'
 import { getItemTierIcon } from '../icon-lookup-table.js'
 import { convertToTimestamp } from '../util/discord-formatting.js'
-import { replyWithError, sendNewlineSplitDiscordTextMessage } from '../util/message-utils.js'
+import { replyWithError } from '../util/message-utils.js'
+import { ArchipelagoSession } from '../archipelago-session.js'
+import { SessionStatus } from '../../types/session-types.js'
+
+interface PlayerProgress {
+  checksDone: number
+  totalChecks: number
+  itemsReceived: number
+}
+
+interface SessionProgress {
+  totalCompletedChecks: number
+  totalChecks: number
+  completedGames: number
+  playerProgress: Record<number, PlayerProgress>
+}
+
+// If there are more than this many players, only show short-form status
+const longFormPlayerLimit = 8
+
+function formatProgressBar (percentage: number, segments = 20) {
+  const totalFilled = Math.floor((percentage / 100) * segments)
+  const filledChar = '█'
+  const partialChar = '▓'
+  const emptyChar = '░'
+  if (totalFilled >= segments) {
+    return filledChar.repeat(segments)
+  }
+  const filledPart = filledChar.repeat(totalFilled)
+  const emptyPart = emptyChar.repeat(segments - totalFilled - 1)
+  return `${filledPart}${partialChar}${emptyPart}`
+}
+
+function formatStaticState (session: ArchipelagoSession) {
+  const connectionStatus = session.isSocketConnected
+    ? `${getItemTierIcon('progression')} Connected as ${session.getCurrentVessel() ?? 'Unknown'}!`
+    : `${getItemTierIcon('trap')} Disconnected`
+  const totalChecks = `Total checks: ${session.staticState.players.reduce((acc, p) => acc + p.game.totalLocations, 0)}`
+  const numPlayers = `Number of players: ${session.staticState.players.length}`
+
+  if (session.staticState.players.length > longFormPlayerLimit) return [connectionStatus, '', totalChecks, numPlayers].join('\n')
+
+  const playerData = session.staticState.players.map(p => {
+    return [
+      `- **${p.slotName}** ${getItemTierIcon('filler')}`,
+      `-# Playing: ${p.game.name}`,
+      `-# Checks: ${p.game.totalLocations}`,
+    ].join('\n')
+  }).join('\n')
+  return [connectionStatus, '', totalChecks, numPlayers, '', playerData].join('\n')
+}
+
+function getProgress (session: ArchipelagoSession, status: SessionStatus): SessionProgress {
+  let totalCompletedChecks = 0
+  let totalChecks = 0
+  let completedGames = 0
+  const playerProgress: Record<number, PlayerProgress> = {}
+  for (const player of session.staticState.players) {
+    const playerChecksDone = status.checksDone[player.slotId].length ?? 0
+    const playerTotalChecks = session.staticState.players.find(p => p.slotId === player.slotId)?.game.totalLocations ?? 1
+    const playerItemsReceived = status.itemsReceived[player.slotId].length ?? 0
+    if (status.playerStatus[player.slotId] === 'Goaled') completedGames += 1
+    playerProgress[player.slotId] = {
+      checksDone: playerChecksDone,
+      totalChecks: playerTotalChecks,
+      itemsReceived: playerItemsReceived,
+    }
+    totalCompletedChecks += playerChecksDone
+    totalChecks += playerTotalChecks
+  }
+  return {
+    totalCompletedChecks,
+    totalChecks,
+    completedGames,
+    playerProgress,
+  }
+}
+
+function formatDynamicState (session: ArchipelagoSession, status: SessionStatus) {
+  const progress = getProgress(session, status)
+  const totalChecksPercent = progress.totalChecks > 0 ? (progress.totalCompletedChecks / progress.totalChecks) * 100 : 100
+  const playersConnected = Object.values(status.playerStatus).filter(s => s !== 'Goaled' && s !== 'Unknown').length
+
+  const replyTokens = []
+  if (session.isSocketConnected) {
+    const currentVessel = session.getCurrentVessel() ?? 'Unknown'
+    replyTokens.push(`${getItemTierIcon('progression')} Connected as __${currentVessel}__!`)
+  } else {
+    replyTokens.push(`${getItemTierIcon('trap')} Disconnected`)
+  }
+  replyTokens.push('')
+  replyTokens.push(formatProgressBar(totalChecksPercent))
+  replyTokens.push(`**${totalChecksPercent.toFixed(2)}% (${progress.totalCompletedChecks} / ${progress.totalChecks})**`)
+  replyTokens.push(`Games completed: **${progress.completedGames} / ${session.staticState.players.length}**`)
+  replyTokens.push(`Players connected: **${playersConnected} / ${session.staticState.players.length}**`)
+  replyTokens.push(`Last Activity: ${convertToTimestamp(status.lastRoomActivity)}`)
+  replyTokens.push('')
+
+  if (session.staticState.players.length > longFormPlayerLimit) return replyTokens.join('\n')
+
+  for (const player of session.staticState.players) {
+    const alias = status.aliases[player.slotId]
+    const nameDisplay = alias ? `**${player.slotName}** "${alias}"` : `**${player.slotName}**`
+
+    const lastActivity = status.lastPlayerActivity[player.slotId]
+    const lastConnection = status.lastPlayerConnection[player.slotId]
+    const playerStatus = (() => {
+      const playerStatus = status.playerStatus[player.slotId] ?? 'Unknown'
+      if (playerStatus === 'Playing') {
+        return `${getItemTierIcon('progression')} ${playerStatus}`
+      } else if (playerStatus === 'Ready' || playerStatus === 'Connected') {
+        if (lastActivity) {
+          return `${getItemTierIcon('useful')} ${playerStatus} ${convertToTimestamp(lastActivity, 'relative')}`
+        }
+        return `${getItemTierIcon('useful')} ${playerStatus}`
+      } else if (playerStatus === 'Goaled') {
+        if (lastActivity) {
+          return ` 🏁 ${convertToTimestamp(lastActivity, 'relative')}`
+        }
+        return ' 🏁'
+      } else {
+        if (lastConnection) {
+          return `${getItemTierIcon('filler')} Last connected ${convertToTimestamp(lastConnection, 'relative')}`
+        }
+        return `${getItemTierIcon('filler')}`
+      }
+    })()
+    replyTokens.push(`- ${nameDisplay} ${playerStatus}`)
+    replyTokens.push(`-# Playing: ${player.game.name}`)
+
+    const playerProgress = progress.playerProgress[player.slotId]
+    const checksPercentage = ((playerProgress.checksDone / playerProgress.totalChecks) * 100).toFixed(2)
+    replyTokens.push(`-# Checks: ${playerProgress.checksDone} / ${playerProgress.totalChecks} (${checksPercentage}%)`)
+    replyTokens.push(`-# Received: ${playerProgress.itemsReceived}`)
+  }
+  return replyTokens.join('\n')
+}
 
 export const status: SessionCommand = {
   name: 'status',
   description: 'Get the current status of the session',
   async execute (message, _args, session) {
-    const status = await session.getCurrentStatus()
-    if (!status) {
+    const initialReply = await message.reply(formatStaticState(session))
+    const currentStatus = await session.getCurrentStatus()
+    if (!currentStatus) {
       await replyWithError(message, 'Unable to get current status. Perhaps the server is down?')
       return
     }
-    const replyTokens = []
-    if (session.isSocketConnected) {
-      const currentVessel = session.getCurrentVessel()
-      if (!currentVessel) {
-        await replyWithError(message, 'Unable to get status, perhaps try again in a bit?')
-        return
-      }
-      replyTokens.push(`${getItemTierIcon('progression')} Connected as ${currentVessel}!`)
-    } else {
-      replyTokens.push(`${getItemTierIcon('trap')} Disconnected`)
-    }
-    replyTokens.push(`Last Activity: ${convertToTimestamp(status.lastRoomActivity)}`)
-    replyTokens.push('')
 
-    for (const player of session.staticState.players) {
-      const alias = status.aliases[player.slotId]
-      const nameDisplay = alias ? `**${player.slotName}** "${alias}"` : `**${player.slotName}**`
-
-      const lastActivity = status.lastPlayerActivity[player.slotId]
-      const lastConnection = status.lastPlayerConnection[player.slotId]
-      const playerStatus = (() => {
-        const playerStatus = status.playerStatus[player.slotId] ?? 'Unknown'
-        if (playerStatus === 'Playing') {
-          return `${getItemTierIcon('progression')} ${playerStatus}`
-        } else if (playerStatus === 'Ready' || playerStatus === 'Connected') {
-          if (lastActivity) {
-            return `${getItemTierIcon('useful')} ${playerStatus} ${convertToTimestamp(lastActivity, 'relative')}`
-          }
-          return `${getItemTierIcon('useful')} ${playerStatus}`
-        } else if (playerStatus === 'Goaled') {
-          if (lastActivity) {
-            return `🏁 ${convertToTimestamp(lastActivity, 'relative')}`
-          }
-          return '🏁'
-        } else {
-          if (lastConnection) {
-            return `${getItemTierIcon('filler')} Last connected ${convertToTimestamp(lastConnection, 'relative')}`
-          }
-          return `${getItemTierIcon('filler')}`
-        }
-      })()
-      replyTokens.push(`- ${nameDisplay} ${playerStatus}`)
-
-      const playerChecksDone = status.checksDone[player.slotId].length ?? 0
-      const playerTotalChecks = session.staticState.players.find(p => p.slotId === player.slotId)?.game.totalLocations ?? 1
-      const playerItemsReceived = status.itemsReceived[player.slotId].length ?? 0
-      const checksPercentage = ((playerChecksDone / playerTotalChecks) * 100).toFixed(2)
-      replyTokens.push(`-# Checks: ${playerChecksDone} / ${playerTotalChecks} (${checksPercentage}%)`)
-      replyTokens.push(`-# Received: ${playerItemsReceived}`)
-    }
-    await sendNewlineSplitDiscordTextMessage(message.reply.bind(message), replyTokens.join('\n'))
+    await initialReply.edit(formatDynamicState(session, currentStatus).substring(0, 2000))
   },
 }
