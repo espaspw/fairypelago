665d87042602b5f4d062ab26f008a2a04e41f2ce
Lady Lumina <67873677+LadyLumina@users.noreply.github.com>
2026-05-14 00:23:51 +0100

    Added Painting support for SM64.

--- src/data/matchers/sm64.ts
+++ src/data/matchers/sm64.ts
@@ -24,4 +24,20 @@ export const sm64Icons: IconMatcher[] = [
     emoji: 'sm64_move',
   },
   { pattern: [/Cannon Unlock.*/], emoji: 'sm64_cannon' },
+  { pattern: ['Painting Unlock WF'], emoji: 'sm64_paintingwf' },
+  { pattern: ['Painting Unlock JRB'], emoji: 'sm64_paintingjrb' },
+  { pattern: ['Painting Unlock CCM'], emoji: 'sm64_paintingccm' },
+  { pattern: ['Painting Unlock LLL'], emoji: 'sm64_paintinglll' },
+  { pattern: ['Painting Unlock WDW'], emoji: 'sm64_paintingwdw' },
+  { pattern: ['Painting Unlock TTM'], emoji: 'sm64_paintingttm' },
+  { pattern: ['Painting Unlock THI'], emoji: 'sm64_paintingthi' },
+  { pattern: ['Painting Unlock TTC'], emoji: 'sm64_paintingttc' },
+  {
+    pattern: [
+      'Painting Unlock SSL',
+      'Painting Unlock DDD',
+      'Painting Unlock SL',
+    ],
+    emoji: 'sm64_paintingbowser',
+  },
 ]
